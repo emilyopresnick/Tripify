663a7616b8bd8f1af06c51cfae632dd409992257
iamashtonfox <ashtonmfox@gmail.com>
2023-07-29 21:20:00 -0400

    aaaaaaaaaaaa

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, redirect, session
 import webbrowser
 from spotipy.oauth2 import SpotifyOAuth
 import json, os
